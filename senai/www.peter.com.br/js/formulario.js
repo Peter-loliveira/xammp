ee1f143d79a9e0c6c842d8bba7233a75cd1f6223
Peter Lange Oliveira <44213765+Peter-loliveira@users.noreply.github.com>
2020-03-05 19:01:21 -0300

    Enviados arquivos do SENAI

---
+++ senai/www.peter.com.br/js/formulario.js
@@ -0,0 +1,18 @@
+var enviar = document.getElementById("enviar")
+enviar.setAttribute("onclick","exibirDadosDigitados()")
+// enviar.onclick(exibirDadosDigitados())
+
+function exibirDadosDigitados(){
+    var nome = document.getElementById("nome").value
+    var email = document.getElementById("email").value
+    var telefone = document.getElementById("telefone").value
+    var mensagem = document.getElementById("mensagem").value
+    
+    alert(`
+    - Nome: ${nome}
+    -E-mail: ${email} 
+    -Telefone: ${telefone}
+    -Mensagem: ${mensagem}`
+    )    
+}
+    
---
+++ senai/www.peter.com.br/php/formulario.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
+    <link rel="stylesheet" href="../css/peter.css">
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Custom styles for this template -->
+    <link href="css/business-casual.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/peter.css">
+</head>
+
+<body>
+    <form class="form-horizontal">
+        <fieldset>
+            <!-- Form Name -->
+            <legend>contato</legend>
+
+            <!-- Text input-->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="nome">Nome:</label>
+                <div class="col-md-5">
+                    <input id="nome" name="nome" type="text" placeholder="Digite seu Nome" class="form-control input-md" required="">
+                    <span class="help-block">O nome é importante identificar o remetente</span>
+                </div>
+            </div>
+
+            <!-- Text input-->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="email">E-mail:</label>
+                <div class="col-md-5">
+                    <input id="email" name="email" type="text" placeholder="Digite seu E-Mail" class="form-control input-md" required="">
+                    <span class="help-block">Precisaremos de seu Email para responde-lo</span>
+                </div>
+            </div>
+
+            <!-- Text input-->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="telefone">Telefone:</label>
+                <div class="col-md-5">
+                    <input id="telefone" name="telefone" type="text" placeholder="Digite seu telefone" class="form-control input-md" required="">
+                    <span class="help-block">Gostariamos de ter seu telefone</span>
+                </div>
+            </div>
+
+            <!-- Textarea -->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="mensagem">Mensagem:</label>
+                <div class="col-md-4">
+                    <textarea class="form-control" id="mensagem" name="mensagem">Digite sue mensagem</textarea>
+                </div>
+            </div>
+
+            <!-- Button -->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="enviar"></label>
+                <div class="col-md-4">
+                    <button id="enviar" name="enviar" class="btn btn-primary">Enviar</button>
+                </div>
+            </div>
+
+        </fieldset>
+    </form>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../js/formulario.js"></script>
+
+</body>
+
+</html>
