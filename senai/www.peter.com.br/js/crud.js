c1aee8af11079a9dd61254fc9aa2cdfe5c3b6ee9
Peter Lange Oliveira <44213765+Peter-loliveira@users.noreply.github.com>
2020-03-07 11:30:54 -0300

    crud Bootstrap

---
+++ senai/www.peter.com.br/js/crud.js
@@ -0,0 +1,11 @@
+var botaoCriarCliente = document.getElementById("salvar")
+botaoCriarCliente.onclick = function(){
+    criarCleinte()
+}
+
+var tabela = document.getElementById("table")
+
+function criarCleinte(){
+    var linha = document.createElement("tr")
+
+}
